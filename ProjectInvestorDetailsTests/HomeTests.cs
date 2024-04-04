3398381bfd4c6656a54f90b7529de4ae63bcbf16
Akim Davies <akimsdavies@gmail.com>
2024-04-04 12:24:30 +0100

    Navigate to correct URL

--- ProjectInvestorDetailsTests/HomeTests.cs
+++ ProjectInvestorDetailsTests/HomeTests.cs
@@ -1,5 +1,7 @@
 using System.Threading.Tasks;
 using Bunit;
+using Bunit.TestDoubles;
+using Microsoft.Extensions.DependencyInjection;
 using ProjectInvestorDetails.Pages;
 using RichardSzalay.MockHttp;
 using Xunit;
@@ -9,7 +11,7 @@ namespace ProjectInvestorDetailsTests;
 public class HomeTests : TestContext
 {
     [Fact]
-    public async Task InvestorPage_ShouldDisplayInvestorData()
+    public async Task HomePage_ShouldDisplayInvestorData()
     {
         var httpClient = Services.AddMockHttpClient();
 
@@ -64,7 +66,7 @@ public class HomeTests : TestContext
     }
 
     [Fact]
-    public async Task InvestorPage_ShouldDisplayLoadingIndicatorWhenDataIsNull()
+    public async Task HomePage_ShouldDisplayLoadingIndicatorWhenDataIsNull()
     {
         var httpClient = Services.AddMockHttpClient();
 
@@ -76,8 +78,38 @@ public class HomeTests : TestContext
             });
 
         var component = RenderComponent<Home>();
-        
+
         var loadingIndicator = component.Find("em");
         Assert.NotNull(loadingIndicator);
+    }
+
+    [Fact]
+    public void ClickingOnInvestorRow_ShouldNavigateToCorrectUrl()
+    {
+        var httpClient = Services.AddMockHttpClient();
+        var navMan = Services.GetRequiredService<FakeNavigationManager>();
+        
+        httpClient
+            .When("http://127.0.0.1:8000/api/investors")
+            .RespondJson(new[]
+            {
+                new
+                {
+                    firm_id = 1,
+                    firm_name = "ABC Investments",
+                    firm_type = "Venture Capital",
+                    date_added = "2024-04-03T00:00:00",
+                    address = "123 Main St"
+                }
+            });
+
+        var component = RenderComponent<Home>();
+        component.WaitForElement("tbody");
+        var firmId = 1;
+        
+        var tableRow = component.Find("tbody tr:contains('ABC Investments')");
+        tableRow.Click();
+        
+        Assert.Equal($"http://localhost/investors/{firmId}", navMan.Uri);
     }
 }
